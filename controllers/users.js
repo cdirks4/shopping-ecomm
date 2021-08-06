313d7f7aecb3bc1e55061759b6196dfaa286c1de
cdirks4 <cdirks4@me.com>
2021-08-06 10:04:41 -0600

    user can now log in and recieve a token

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 module.exports = {
 	create,
 	index,
+	login,
 };
 
 async function index(req, res) {
@@ -19,7 +20,6 @@ async function index(req, res) {
 
 async function create(req, res) {
 	try {
-		console.log('hi');
 		const user = await User.create(req.body);
 		const token = createJWT(user);
 		res.json(token);
@@ -28,6 +28,26 @@ async function create(req, res) {
 		res.status(400).json(err);
 	}
 }
+
+async function login(req, res) {
+	try {
+		console.log('hi');
+		const user = await User.findOne({ email: req.body.email });
+		console.log(user);
+		if (!user) throw new Error('No user with corresonding email');
+		const match = await bcrypt.compare(req.body.password, user.password);
+		if (match) {
+			const token = createJWT(user);
+			res.json(token);
+		} else {
+			throw new Error('password did not match the username');
+		}
+	} catch (err) {
+		res.json(err);
+	}
+}
+
+async function update(req, res) {}
 
 function createJWT(user) {
 	return jwt.sign(
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const usersCtrl = require('../../controllers/users');
 
 router.post('/', usersCtrl.create);
+router.post('/login', usersCtrl.login);
 router.get('/', usersCtrl.index);
 module.exports = router;
